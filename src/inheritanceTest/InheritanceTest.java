7421db8b106f9268055e4a67ea7e89ad7ce74e25
pratikshaK48 <pratikshakokare4@gmail.com>
2021-04-16 18:17:48 +0530

    new commit

---
+++ src/inheritanceTest/InheritanceTest.java
@@ -0,0 +1,17 @@
+package inheritanceTest;
+
+public class InheritanceTest {
+
+	public InheritanceTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Cat c = new Cat();
+      c.meow();
+      c.eat();
+      
+	}
+
+}
